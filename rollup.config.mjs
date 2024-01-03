152ba897264338616f6fc135474209361dd0419d
rameel <rameel-b@hotmail.com>
2024-01-04 01:22:53 +0600

    Add some rollup plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,75 @@
-import alias from "@rollup/plugin-alias";
 import path from "path";
+import alias from "@rollup/plugin-alias";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import size from "rollup-plugin-bundle-size";
+import terser from "@rollup/plugin-terser";
+import {
+    fileURLToPath
+} from "url";
 
-import { fileURLToPath } from "url";
+const __SRC = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "src");
 
-export default {
-    input: "src/index.js",
+const production = process.env.NODE_ENV === "production";
+
+const terserOptions = {
     output: {
+        comments: false
+    },
+    compress: {
+        passes: 5,
+        ecma: 2020,
+        drop_console: true,
+        drop_debugger: true,
+        pure_getters: true,
+        arguments: true,
+        unsafe_comps: true,
+        unsafe_math: true,
+        unsafe_methods: true
+    }
+};
+
+export default [{
+    input: "src/index.js",
+    treeshake: "smallest",
+    output: [{
         file: "dist/alpinejs-booster.js",
         format: "iife",
         sourcemap: true
-    },
+    }, production && {
+        file: "dist/alpinejs-booster.min.js",
+        format: "iife",
+        plugins: [terser(terserOptions)]
+    }],
     plugins: [
+        resolve(),
+        production && size(),
+        production && replace({
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+            preventAssignment: true
+        }),
         alias({
             entries: [
-                { find: "@", replacement: path.join(path.dirname(fileURLToPath(import.meta.url)), "src") }
+                { find: "@", replacement: __SRC }
             ]
         })
-    ]
-}
+    ]},
+    production && {
+        input: "src/plugins/index.js",
+        output: {
+            file: "dist/alpinejs-booster.esm.js",
+            format: "esm"
+        },
+        plugins: [
+            resolve(),
+            alias({
+                entries: [
+                    { find: "@", replacement: __SRC }
+                ]
+            }),
+            size()
+        ]
+    }
+].filter(Boolean);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { destroy } from "@/plugins/index.js";
-import { format } from "@/plugins/index.js";
-import { fragment } from "@/plugins/index.js";
-import { match } from "@/plugins/index.js";
-import { router } from "@/plugins/index.js";
-import { template } from "@/plugins/index.js";
-import { when } from "@/plugins/index.js";
+import { destroy } from "@/plugins";
+import { format } from "@/plugins";
+import { fragment } from "@/plugins";
+import { match } from "@/plugins";
+import { router } from "@/plugins";
+import { template } from "@/plugins";
+import { when } from "@/plugins";
 
 document.addEventListener("alpine:init", () => {
     Alpine.plugin([
