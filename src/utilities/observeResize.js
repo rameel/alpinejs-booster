b20003b0afaf54411304224bbe01f87c5bb81307
rameel <rameel-b@hotmail.com>
2024-01-14 05:22:32 +0600

    Formatting

--- src/utilities/observeResize.js
+++ src/utilities/observeResize.js
@@ -3,9 +3,11 @@ let observable = null;
 
 export function observeResize(el, listener) {
     observable ??= new ResizeObserver(entries => {
-        for (const e of entries)
-            for (const callback of e.target[key].values())
+        for (const e of entries) {
+            for (const callback of e.target[key].values()) {
                 callback(e);
+            }
+        }
     });
 
     el[key] || (el[key] = new Set());
