20c9730d613d2445434d17d66324db3177a837d0
rameel <rameel-b@hotmail.com>
2024-01-19 05:23:10 +0600

    Expose RoutePattern class

--- src/plugins/utilities.js
+++ src/plugins/utilities.js
@@ -1,8 +1,11 @@
+import { RoutePattern } from "@/plugins/router/RoutePattern";
 import { createGetter, createSetter } from "@/utilities/evaluator";
 import { registerHotkey } from "@/utilities/registerHotkey";
 import { watch } from "@/utilities/watch";
 
 export default function(alpine) {
+    window.RoutePattern = RoutePattern;
+
     Object.assign(alpine, {
         createGetter,
         createSetter,
